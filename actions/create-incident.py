fe653f073a8ec06a36a48a0ef8c26b3abff31cd8
KONAKESHAVAREDDY <104962709+KONAKESHAVAREDDY@users.noreply.github.com>
2023-06-28 12:25:15 +0530

    Update create-incident.py

--- actions/create-incident.py
+++ actions/create-incident.py
@@ -1,23 +1,33 @@
-from st2common.runners.base_action import Action
 import requests
+import json
+
+def run(stackstorm_event):
+    # ServiceNow API endpoint
+    url = 'https://your-instance.service-now.com/api/now/table/incident'
+
+    # ServiceNow API credentials
+    username = 'admin'
+    password = 'grBDH*m7bH0@'
 
 
-class CreateIncidentAction(Action):
-    def run(self, instance_name):
-        url = "https://" + instance_name + ".servicenow.com/api/now/table/incident"
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": "Bearer your-servicenow-api-token",
-        }
-        payload = {
-            "short_description": "New incident created by StackStorm",
-            "description": "This incident was created by StackStorm integration",
-            "category": "Hardware",
-            "priority": "3",
-        }
+    # JSON payload for creating an incident
+    payload = {
+        'short_description': 'new ticket',
+        'description': 'Creating new tcket on servicenow'
+    }
 
-        response = requests.post(url, json=payload, headers=headers)
+    # Set the headers for the request
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    # Make the API call to create an incident
+    try:
+        response = requests.post(url, auth=(username, password), headers=headers, data=json.dumps(payload))
         response.raise_for_status()
-
-        return response.json()["result"]["number"]
+        incident_data = response.json()
+        sys_id = incident_data.get('result', {}).get('sys_id')
+        return {'sys_id': sys_id}
+    except requests.exceptions.RequestException as e:
+        return str(e)
