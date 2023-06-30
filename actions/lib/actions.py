88ec6a38ceeed30942cce03601f261a77dc6344d
KONAKESHAVAREDDY <104962709+KONAKESHAVAREDDY@users.noreply.github.com>
2023-06-30 14:10:00 +0530

    Update actions.py

--- actions/lib/actions.py
+++ actions/lib/actions.py
@@ -1,35 +1,18 @@
 from st2common.runners.base_action import Action
-import requests
-import json
-class CreateIncidentAction(Action):
+import pysnow as sn
+class BaseAction(Action):
+    def __init__(self, config=None, action_service=None):
+        super(BaseAction, self).__init__(config, action_service)
+        self.client = self._get_client()
 
-    def run(stackstorm_event):
-    # ServiceNow API endpoint
-        url = 'https://your-instance.service-now.com/api/now/table/incident'
+    def _get_client(self):
+        instance_name = self.config['instance_name']
+        username = self.config['username']
+        password = self.config['password']
 
-    # ServiceNow API credentials
-        username = 'admin'
-        password = 'grBDH*m7bH0@'
+        client = sn.Client(instance=instance_name, user=username, password=password)
 
+        if 'custom_params' in self.config and isinstance(self.config['custom_params'], dict):
+            client.parameters.add_custom(self.config['custom_params'])
 
-    # JSON payload for creating an incident
-        payload = {
-            'short_description': 'new ticket',
-            'description': 'Creating new tcket on servicenow'
-        }
-
-    # Set the headers for the request
-        headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-
-    # Make the API call to create an incident
-        try:
-            response = requests.post(url, auth=(username, password), headers=headers, data=json.dumps(payload))
-            response.raise_for_status()
-            incident_data = response.json()
-            sys_id = incident_data.get('result', {}).get('sys_id')
-            return {'sys_id': sys_id}
-        except requests.exceptions.RequestException as e:
-            return str(e)
+        return client
