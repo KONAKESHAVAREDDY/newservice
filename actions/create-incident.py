baad4942d23660b334cbefcf38c5d8ed8e19a0f9
KONAKESHAVAREDDY <104962709+KONAKESHAVAREDDY@users.noreply.github.com>
2023-07-04 14:32:42 +0530

    Update create-incident.py

--- actions/create-incident.py
+++ actions/create-incident.py
@@ -1,11 +1,29 @@
-from lib.actions import BaseAction
 
+import boto3
 
-class CreateIncidentAction(BaseAction):
-    def run(self, payload):
-        s = self.client
+def start_ec2_instance():
+    # Create an EC2 client
+    ec2_client = boto3.client(
+        'ec2',
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        region_name=region
+    )
 
-        path = '/table/incident'
-        response = s.resource(api_path=path).create(payload=payload)
-        sys_id= response.get('sys_id')
-        return sys_id
+    # Start the EC2 instance
+    response = ec2_client.start_instances(
+        InstanceIds=[instance_id]
+    )
+
+    return response
+
+# Read parameters from StackStorm context
+instance_id = '{{i-0ed11475162ce1791}}'
+access_key_id = '{{AKIASZH77AMGYYERE34B}}'
+secret_access_key = '{{HKYxXhaDt2SlNacUdLdYZ1MgJ+OoCX1FfWgFhENu}}'
+region = 'us-east-1'  # Update with your specific region
+
+# Start the EC2 instance
+response = start_ec2_instance()
+
+print(response)
