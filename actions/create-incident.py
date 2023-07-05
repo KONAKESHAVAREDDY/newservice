bdc87e9f1cd30ccf87b1f261a4a58dc7ee95d814
KONAKESHAVAREDDY <104962709+KONAKESHAVAREDDY@users.noreply.github.com>
2023-07-05 10:06:15 +0530

    Update create-incident.py

--- actions/create-incident.py
+++ actions/create-incident.py
@@ -18,9 +18,9 @@ def start_ec2_instance():
     return response
 
 # Read parameters from StackStorm context
-instance_id = '{{i-0ed11475162ce1791}}'
-access_key_id = '{{AKIASZH77AMGYYERE34B}}'
-secret_access_key = '{{HKYxXhaDt2SlNacUdLdYZ1MgJ+OoCX1FfWgFhENu}}'
+instance_id = '{{}}'
+access_key_id = '{{}}'
+secret_access_key = '{{}}'
 region = 'us-east-1'  # Update with your specific region
 
 # Start the EC2 instance
